43837eb0f056860a0d5bb3e214b85978bab1f187
KnivesTix <joola1992@gmail.com>
2020-07-07 09:04:33 +0300

    test

---
+++ src/app/ecommerce-item/ecommerce-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce-item',
+  templateUrl: './ecommerce-item.component.html',
+  styleUrls: ['./ecommerce-item.component.css']
+})
+export class EcommerceItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
